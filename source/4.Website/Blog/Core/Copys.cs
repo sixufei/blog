17702d2c9551055e082993df4d27b6eb38582a4e
jiangzhiwei <978025952@qq.com>
2017-11-23 23:52:51 +0800

    1

---
+++ source/4.Website/Blog/Core/Copys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Core
+{
+    public class Copys
+    {
+        public static string ProtalName = "个人博客-江志伟";
+    }
+}
